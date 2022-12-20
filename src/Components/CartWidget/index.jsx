00b5409b6492463aef4c183b5ce2cbdab45a3c8e
Malinowsk <juanrago2015@gmail.com>
2022-12-20 03:41:07 -0300

    PrimerEntrega Rago

--- src/Components/CartWidget/index.jsx
+++ src/Components/CartWidget/index.jsx
@@ -3,7 +3,7 @@ import { ShoppingCartOutlined } from '@ant-design/icons';
 
 function CartWidget(props) {
   return (
-    <div><ShoppingCartOutlined className="cartWidget-container"/>{props.cantidad}</div>
+    <div className='cart-container'><ShoppingCartOutlined className="cartWidget-container"/>{props.cantidad}</div>
   )
 }
 
--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,23 +1,34 @@
 import './style.css';
 import CartWidget from '../CartWidget';
 import logo from '../../assets/logo.png';
+import { MenuOutlined } from '@ant-design/icons';
+
+
+function toggerInvi(){
+    document.querySelector("#uno").classList.toggle("invisibilidad");
+
+}
+
 
 function NavBar() {
     return (
         <header className="header"> 
+            <div  class="button-hamb" onClick={toggerInvi}>
+                <MenuOutlined />
+            </div>
             <div className="space-between">
                 <img className="imagen-logo" src={logo} alt="logo-cerveza"/>
                 <p className="titulo-logo">VIVA<br/>LA BIRRA</p>
             </div>
+            <nav className='content'>  
+                <ul id='uno' className='space-between menu invisibilidad' >
+                    <li className='item-nav'><a href='http:/'>Home</a></li>
+                    <li className='item-nav'><a href='http:/'>Artesanales</a></li>
+                    <li className='item-nav'><a href='http:/'>Comerciales</a></li>
+                    <li className='item-nav'><a href='http:/'>Importadas</a></li>
+                </ul>
+            </nav>
             <div className="space-between">
-                <nav>  
-                    <ul className='space-between nav'>
-                        <li className='item-nav'><a>Home</a></li>
-                        <li className='item-nav'><a>Artesanales</a></li>
-                        <li className='item-nav'><a>Comerciales</a></li>
-                        <li className='item-nav'><a>Importadas</a></li>
-                    </ul>
-                </nav>
                 <CartWidget cantidad="10"></CartWidget>
             </div>
         </header>
